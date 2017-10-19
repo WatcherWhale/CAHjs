90f8670cff249a20be39048e201dd1cebac29304
Mathias Maes <mathiasmaesdev@gmail.com>
2017-10-19 18:23:01 +0200

    Add game logic

--- utils/game.js
+++ utils/game.js
@@ -4,11 +4,12 @@ var CardCast = require('./modules/cardcast.js');
 
 function Game(io)
 {
+    //Setup variables
     this.id = shortid.generate();
 
     this.admin = null;
+    this.czar = null;
     this.players = [];
-    this.spectators = [];
 
     this.options = require('./gameOptions.json');
 
@@ -17,13 +18,18 @@ function Game(io)
 
     this.cards = {"calls":[],"responses":[]};
     this.decks = [];
+
+    this.callloops = 0;
+    this.drawcards = 0;
 }
 
 Game.prototype.SetupGameServer = function(io)
 {
+    //Start game server
     this.server = io.of('/game/' + id);
     this.server.on('connection',function(socket)
     {
+        //Add player to list
         this.players.push(socket);
         this.admin = this.players[0];
 
@@ -35,18 +41,21 @@ Game.prototype.SetupGameServer = function(io)
                 this.server.close();
         });
 
+        //Add deck
         socket.on("deck",function(deckid)
         {
             if(isAdmin(socket))
                 this.LoadDeck(socket,deckid);
         });
 
+        //Change Password
         socket.on("password",function(pass)
         {
             if(isAdmin(socket))
                 this.options.password = pass;
         });
 
+        //Check if admin
         function isAdmin(s)
         {
             if(this.admin == s)
@@ -74,6 +83,7 @@ Game.prototype.LoadDeck = function(socket,deckid)
 
 Game.prototype.StartGame = function()
 {
+    this.czar =
     decks.forEach(function(deck) 
     {
         this.cards.calls = deck.calls;
@@ -83,12 +93,60 @@ Game.prototype.StartGame = function()
     this.cards.calls = shuffle(this.cards.calls);
     this.cards.responses = shuffle(this.cards.responses);
 
+    this.callloops = this.cards.calls.length;
+
     this.players.forEach(function(player)
     {
         var cards = this.cards.calls.splice(0,10);
-        player.emit(cards);
+        player.emit("cards",cards);
     });
 
+
+    this.NextCallCard();
+}
+
+Game.prototype.NextCallCard = function()
+{
+    var card = this.cards.splice(0,1);
+    this.cards.push(card);
+
+    this.callloops--;
+
+    if(this.callloops == 0)
+    {
+        this.cards.calls = shuffle(this.cards.calls);
+    }
+
+    this.NextCzar();
+    this.server.broadcast("callcard",card);
+    
+    this.players.forEach(function(player)
+    {
+        var cards = this.cards.calls.splice(0,drawcards);
+        player.emit("cards",cards);
+    });
+
+    this.drawcards = card.numResponses();
+}
+
+Game.prototype.NextCzar = function()
+{
+    if(this.czar == null)
+    {
+        var r = Math.round(Math.random() * (this.players.length - 1));
+
+        this.czar = this.players[r];
+    }
+    else
+    {
+        var i = this.players.indexOf(this.czar) + 1;
+
+        if(i == this.players.length) i = 0;
+
+        this.czar = this.players[i];
+    }
+
+    this.czar.emit("czar");
 }
 
 module.exports = Game;
