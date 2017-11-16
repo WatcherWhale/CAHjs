90b82843e606b14236271a806cd5aecc0cef91ff
Mathias Maes <mathiasmaesdev@gmail.com>
2017-11-16 17:49:03 +0100

    Cha: Rewrite cardcast api calls

--- Server/modules/cardcast.js
+++ Server/modules/cardcast.js
@@ -1,23 +1,92 @@
 var fs = require('fs');
-var CardcastAPI = require('cardcast-api').CardcastAPI;
-
-var api = new CardcastAPI();
+var https = require('https');
 
 function GetDeck(deckId,callback)
 {
-    try
+    GetDeckInfo(deckId,function(err,deckinfo)
     {
-        api.deck(deckId).then(function(deck)
+        if(err)
         {
-            callback(null,deck);
+            callback(err,deckinfo);
+            return;
+        }
 
-            //fs.writeFile('./cache/' + deckId + '.json',deck);
+        ObtainCards(deckinfo,function(err,cards)
+        {
+            if(err)
+            {
+                callback(err,null);
+                return;
+            }
+
+            deckinfo['calls'] = cards['calls'];
+            deckinfo['responses'] = cards['responses'];
+
+            callback(null,deckinfo);
         });
-    }
-    catch(ex)
-    {
-        callback(ex,null);
-    }
+    });
 }
 
 module.exports.GetDeck = GetDeck;
+
+var apiUrl = "https://api.cardcastgame.com/v1/";
+
+function GetDeckInfo(deck,callback)
+{
+    var buffer = new Buffer("");
+
+    https.get("https://api.cardcastgame.com/v1/decks/" + deck,function(res)
+    {
+        res.on('error',function(err)
+        {
+            callback(err,null);
+        });
+
+        if(res.statusCode == 404)
+        {
+            callback(new Error("Deck was not found." + res.statusCode),null);
+        }
+        else
+        {
+            res.on('data', function(data)
+            {
+                buffer = Buffer.concat([buffer, data], buffer.length + data.length);
+            });
+
+            res.on('end',function()
+            {
+                var data = buffer.toJSON();
+                callback(null,data); 
+            });
+        }
+    });
+}
+
+function ObtainCards(deckinfo)
+{
+    https.get("https://api.cardcastgame.com/v1/decks/" + deckinfo.code + "/cards",function(res)
+    {
+        res.on('error',function(err)
+        {
+            callback(err,null);
+        });
+
+        if(res.statusCode == 404)
+        {
+            callback(new Error("Cards could not be loaded." + res.statusCode),null);
+        }
+        else
+        {
+            res.on('data', function(data)
+            {
+                buffer = Buffer.concat([buffer, data], buffer.length + data.length);
+            });
+
+            res.on('end',function()
+            {
+                var data = buffer.toJSON();
+                callback(null,data); 
+            });
+        }
+    });
+}
