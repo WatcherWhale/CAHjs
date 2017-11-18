8fd9529cc1824973fba524bc8572778e1f1f497b
Mathias Maes <mathiasmaesdev@gmail.com>
2017-11-18 20:08:50 +0100

    Major redesign in menu

--- Server/static/js/menu.js
+++ Server/static/js/menu.js
@@ -5,12 +5,12 @@ socket.on('menuupdate',function(games)
     for (var i = 0; i < games.length; i++) 
     {
         var game = games[i];
-        var mi = menuitem;
+        var mi = newmenuitem;
         
-        mi = mi.replace("%TITLE%",game.title);
-        mi = mi.replace("%PLAYERS%",game.players);
+        mi = mi.replaceAll("%TITLE%",game.title);
+        mi = mi.replaceAll("%PLAYERS%",game.players);
         mi = mi.replaceAll("%GAMEID%",game.id);
-        mi = mi.replace("%PASSWORD%",game.password);
+        mi = mi.replaceAll("%PASSWORD%",game.password);
 
         $(".menucontainer").append(mi);
     }
@@ -29,14 +29,14 @@ socket.on("gameupdate",function(game)
     }
     else if(gameEl.length == 0)
     {
-        var mi = menuitem;
+        var mi = newmenuitem;
         
-        mi = mi.replace("%TITLE%",game.title);
-        mi = mi.replace("%PLAYERS%",game.players);
+        mi = mi.replaceAll("%TITLE%",game.title);
+        mi = mi.replaceAll("%PLAYERS%",game.players);
         mi = mi.replaceAll("%GAMEID%",game.id);
-        mi = mi.replace("%PASSWORD%",game.password);
+        mi = mi.replaceAll("%PASSWORD%",game.password);
 
-        $(".menucontainer").append(mi);
+        $(".menucontainer").html(mi + $(".menucontainer").html());
     }
 });
 
@@ -48,3 +48,8 @@ socket.on("removegame",function(id)
 var menuitem = '<div class="menuitem" id="%GAMEID%"><div class="title">%TITLE%</div>' +
 '<div class="info"><div class="row players"><b>Players:</b> %PLAYERS%</div><div class="row password"><b>Password:</b> %PASSWORD%</div>' +
 '</div><a class="waves-effect waves-light btn green" onclick="JoinGame(\'%GAMEID%\')">Join Game</a></div>';
+
+var newmenuitem = '<div class="card box z-depth-2 menuitem" id="%GAMEID%">' +
+'<div class="card-content"><span class="card-title title">%TITLE%</span>' +
+'<div class="info"><div class="row players"><b>Players:</b> %PLAYERS%</div><div class="row password"><b>Password:</b> %PASSWORD%</div>' +
+'</div><div class="card-action"><a onclick="JoinGame(\'%GAMEID%\')" href="#!">Join Game</a></div></div></div>';
